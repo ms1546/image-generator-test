e9f832810daf2fa2d83af19f34b7329e853273e2
ms1546 <123814410+ms1546@users.noreply.github.com>
2024-09-05 05:13:53 +0000

    fixx

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders the app title', () => {
   render(<App />);
-  
   const titleElement = screen.getByText(/Image Generation with OpenAI/i);
   expect(titleElement).toBeInTheDocument();
 });
